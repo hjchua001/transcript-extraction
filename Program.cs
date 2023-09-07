854105d110ca03200b883ccea0aeff152fc50e75
Han Jon Chua <hj.chua@ogglobal.net>
2023-09-07 23:22:13 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+string jsonFilePath = @"C:\Users\User\Downloads\transcripts_18_Jul_10.33_am__Sepang_Low_Bitrate.mp3-20230907014411.json";
+string jsonData = File.ReadAllText(jsonFilePath);
+JsonObject jsonObject = JsonSerializer.Deserialize<JsonObject>(jsonData);
+JsonArray resultsArray = (JsonArray)jsonObject["results"];
+
+foreach (JsonObject resultObject in resultsArray)
+{
+    string transcript = (string)resultObject["alternatives"][0]["transcript"];
+    Console.WriteLine(transcript);
+}
